6b3f8c46325f99b46b1427025a28aa52b3fc79ea
Vahan Hovhannisyan <vaa.hovanisyan@gmail.com>
2024-04-15 15:43:00 +0400

    done

---
+++ src/router.js
@@ -0,0 +1,45 @@
+// router pages 
+import Navigo from "navigo";
+import getPageContainer from "/src/components/pageContainer";
+
+export const router = new Navigo('/', { hash: true });
+
+const main = getPageContainer();
+
+export function renderPages() {
+
+  // router main page
+  router.on('/', async () => {
+    main.innerHTML = '';
+    const mainModule = await import('./pages/main');
+    const mainPage = mainModule.getMainPage();
+    main.append(mainPage);
+  });
+
+  // router Favorite page
+  router.on('/favorite', async () => {
+    main.innerHTML = '';
+    const favoriteModule = await import('./pages/favorite');
+    const favoritePage = favoriteModule.getFavoritePage();
+    main.append(favoritePage);
+  });
+
+  // router Profile page
+  router.on('/profile', async () => {
+    main.innerHTML = '';
+    const profileModule = await import('./pages/profile');
+    const profilePage = profileModule.getProfilePage();
+    main.append(profilePage);
+  });
+
+  // router notFound page
+  router.notFound(async () => {
+    main.innerHTML = '';
+    const notFoundModule = await import('./pages/404.js');
+    const notFoundPage = notFoundModule.getNotFoundPage();
+    main.append(notFoundPage);
+  })
+
+  // router pages
+  router.resolve();
+}
